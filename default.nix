644ac61cb556e57d79bc4778c9fab6d186e6d40b
Cole Scott <colescottsf@gmail.com>
2018-04-24 20:27:51 -0700

    Add basic UEFI code to load memory map

--- default.nix
+++ default.nix
@@ -6,7 +6,15 @@ stdenv.mkDerivation rec {
     name = name;
     paths = buildInputs;
   };
-  buildInputs = [
+  buildInputs = with pkgs; [
     (import ./tools/gcc.nix)
+    (import ./tools/binutils.nix)
+    qemu OVMF
+
+    gnu-efi
+    parted mtools # For UEFI image building
   ];
+
+  GNU_EFI_DIR=pkgs.gnu-efi; # GNU EFI dir for headers
+  OVMF_DIR="${pkgs.OVMF.fd}/FV"; # OVMF dir for qemu bios
 }
